56991fe1ad85348be21d44d42f9e0cc9d13a1a87
Your Name <you@example.com>
2021-07-06 23:34:34 +0530

    binary-veda

--- src/experience.js
+++ src/experience.js
@@ -6,7 +6,7 @@ export default function Tag(){
         <p className='p2'>Experience the freedom to build</p>
         <p className='p2 p3'>features, right away.</p> 
         <p className='p4'>You can now harness components of our platform and build powerful</p>
-        <p className='p5'>integrations for our 2 million users-or simply just for your team.</p>
+        <p className='p5'>integrations for our 2 million users-or simply just for your team</p>
     </div>
     )
 }
