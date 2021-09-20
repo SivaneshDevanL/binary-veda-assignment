36251f0bcec6c157da955359a7c00dd0d880910d
SivaneshDevanL <71081217+SivaneshDevanL@users.noreply.github.com>
2021-09-20 22:45:49 +0530

    Update veda.js

--- src/veda.js
+++ src/veda.js
@@ -1,186 +1,30 @@
 import React from 'react';
 import './veda.css';
-// import './fontawesome-free-5.15.3-web/css/all.css'
-// import img from './Technical Assignment (Frontend)/assets/logo-white.svg'
-// import img1 from './Technical Assignment (Frontend)/assets/illustration.png'
-// import img2 from './Technical Assignment (Frontend)/assets/graphQL.png'
-// import img3 from './Technical Assignment (Frontend)/assets/superchargeWorkflow.png'
-// import img4 from './Technical Assignment (Frontend)/assets/apiTeam@2x-5a72bbd0df3f17c4851dca773b2b1b8f-3ad8f.png'
-// import img5 from './Technical Assignment (Frontend)/assets/showcase.png'
-// import img6 from './Technical Assignment (Frontend)/assets/botbot.png'
-// import Header from './header';
-// import Illustration from './illustration';
-// import Tag from './experience';
-// import ApiTeam from './API team';
-// import Showcase from './showcase';
-// import SlackBot from './Slack-bot';
-// import Footer from './footer';
+import './fontawesome-free-5.15.3-web/css/all.css'
+import img from './Technical Assignment (Frontend)/assets/logo-white.svg'
+import img1 from './Technical Assignment (Frontend)/assets/illustration.png'
+import img2 from './Technical Assignment (Frontend)/assets/graphQL.png'
+import img3 from './Technical Assignment (Frontend)/assets/superchargeWorkflow.png'
+import img4 from './Technical Assignment (Frontend)/assets/apiTeam@2x-5a72bbd0df3f17c4851dca773b2b1b8f-3ad8f.png'
+import img5 from './Technical Assignment (Frontend)/assets/showcase.png'
+import img6 from './Technical Assignment (Frontend)/assets/botbot.png'
+import Header from './header';
+import Illustration from './illustration';
+import Tag from './experience';
+import ApiTeam from './API team';
+import Showcase from './showcase';
+import SlackBot from './Slack-bot';
+import Footer from './footer';
 
 export default function App(){
     return(<>
-        {/*     <Header img={img}/>
+            <Header img={img}/>
         <Illustration img1={img1}/>
         <Tag/>
         <ApiTeam img2={img2} img3={img3} img4={img4}/>
         <Showcase img5={img5}/>
         <SlackBot img6={img6}/>
-        <Footer img={img}/> */}
- <div className="pricing-table">
-   <div className="box">
-       <h5>Basic</h5>
-       <table>
-           <tr>
-               <td className="price">$9</td>
-               <td>/month</td>
-           </tr>
-       </table>
-       <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
-       <button>Sign Up</button>
-   </div>
-   <div className="box">
-       <h5>Basic</h5>
-       <table>
-           <tr>
-               <td className="price">$9</td>
-               <td>/month</td>
-           </tr>
-       </table>
-       <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
-       <button>Sign Up</button>
-   </div>
-   <div className="box">
-       <h5>Basic</h5>
-       <table>
-           <tr>
-               <td className="price">$9</td>
-               <td>/month</td>
-           </tr>
-       </table>
-       <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
-       <button>Sign Up</button>
-   </div>
-   <div className="box">
-       <h5>Basic</h5>
-       <table>
-           <tr>
-               <td className="price">$9</td>
-               <td>/month</td>
-           </tr>
-       </table>
-       <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
-       <button>Sign Up</button>
-   </div>
-   <div className="box">
-      <h5>Basic</h5>
-      <table>
-          <tr>
-              <td className="price">$5</td>
-              <td>/month</td>
-          </tr>
-      </table>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
-      <button>Sign Up</button>
-  </div>
-  <div className="box">
-   <h5>Basic</h5>
-   <table>
-       <tr>
-           <td className="price">$9</td>
-           <td>/month</td>
-       </tr>
-   </table>
-   <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
-   <button>Sign Up</button>
-</div>
-<div className="box">
-   <h5>Basic</h5>
-   <table>
-       <tr>
-           <td className="price">$9</td>
-           <td>/month</td>
-       </tr>
-   </table>
-   <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
-   <button>Sign Up</button>
-</div>
-<div className="box">
-   <h5>Basic</h5>
-   <table>
-       <tr>
-           <td className="price">$9</td>
-           <td>/month</td>
-       </tr>
-   </table>
-   <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
-   <button>Sign Up</button>
-</div>
-<div className="box">
-   <h5>Basic</h5>
-   <table>
-       <tr>
-           <td className="price">$9</td>
-           <td>/month</td>
-       </tr>
-   </table>
-   <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
-   <button>Sign Up</button>
-</div>
-<div className="box">
-   <h5>Basic</h5>
-   <table>
-       <tr>
-           <td className="price">$9</td>
-           <td>/month</td>
-       </tr>
-   </table>
-   <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
-   <button>Sign Up</button>
-</div>
-<div className="box">
-   <h5>Basic</h5>
-   <table>
-       <tr>
-           <td className="price">$9</td>
-           <td>/month</td>
-       </tr>
-   </table>
-   <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
-   <button>Sign Up</button>
-</div>
-<div className="box">
-   <h5>Basic</h5>
-   <table>
-       <tr>
-           <td className="price">$9</td>
-           <td>/month</td>
-       </tr>
-   </table>
-   <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
-   <button>Sign Up</button>
-</div>
-<div className="box">
-   <h5>Basic</h5>
-   <table>
-       <tr>
-           <td className="price">$9</td>
-           <td>/month</td>
-       </tr>
-   </table>
-   <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
-   <button>Sign Up</button>
-</div>
-<div className="box">
-    <h5>Basic</h5>
-    <table>
-        <tr>
-            <td className="price">$8</td>
-            <td>/month</td>
-        </tr>
-    </table>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
-    <button>Sign Up</button>
- </div>
-</div>
+        <Footer img={img}/> 
         </>
     )
 }
